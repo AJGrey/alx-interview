5b827b1196f68b52dba314ce933fc3d648d75df7
OlasunkanmiAjayi <greyajayi@yahoo.com>
2023-07-03 07:44:45 -0700

    fix

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,17 +3,20 @@
 A function that prints Pascal's triangle
 """
 
-
 def pascal_triangle(n):
-    """ Creates the triangle"""
     if n <= 0:
         return []
 
-    triangle = []
-    for row_num in range(n):
-        current_row = [1] * (row_num + 1)
-        for col_num in range(1, row_num):
-            current_row[col_num] = triangle[row_num - 1][col_num - 1] + triangle[row_num - 1][col_num]
-        triangle.append(current_row)
-        print(current_row)
+    triangle = [[1]]  # Initialize with the first row
 
+    for i in range(1, n):
+        row = [1]  # First element of each row is always 1
+
+        # Calculate the elements in between
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+
+        row.append(1)  # Last element of each row is always 1
+        triangle.append(row)
+
+    return triangle
